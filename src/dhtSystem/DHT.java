9239dd7ce819604c24f093c7ba2c074d7ecc0e09
Alejandro Rodríguez Calzado <alerguez94@gmail.com>
2017-01-24 12:28:28 +0100

    Added put functionality to DHT class. The user can now enter the node to which he wants to send the requests

--- src/dhtSystem/DHT.java
+++ src/dhtSystem/DHT.java
@@ -11,6 +11,7 @@ import org.jgroups.conf.ClassConfigurator;
 
 import dhtSystem.messages.DataMessage;
 import dhtSystem.messages.GetDataMessage;
+import dhtSystem.messages.PutDataMessage;
 import dhtSystem.messages.TypeOfMessageHeader;
 
 
@@ -25,7 +26,8 @@ public class DHT extends ReceiverAdapter {
 	private static int select = -1;
 	
 	private JChannel channel;
-	private View view;
+	private View view = null;
+	private Address destAddress = null;;
 	
 	//TODO Allow the user to set the requested node by command line options
 
@@ -33,7 +35,15 @@ public class DHT extends ReceiverAdapter {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		DHT dht = DHT.getInstance();
+		DHT dht;
+		
+		if (args.length == 1)
+			dht = DHT.getInstance(args[0]);
+		else
+			dht = DHT.getInstance(null);
+		
+		//Address add = UUID.fromString("Alejandro-10155");
+		//System.out.println(add);
 		
 		synchronized (dht) {
 			while(select != 0){
@@ -72,37 +82,46 @@ public class DHT extends ReceiverAdapter {
 		}
 	}
 
-	private DHT() {
+	private DHT(String addr) {	
 		String props="mping.xml"; 
-        String name=null;
-        
-        try {
-        	// Connect to the group
-        	this.start(props, name);
-        } catch (Exception e) {
-        	System.err.println("Error during connection.");
-        }
-        
-        ClassConfigurator.add(Common.TYPE_HEADER_MAGIC_ID, TypeOfMessageHeader.class);
+		String name=null;
+
+		try {
+			// Connect to the group
+			this.start(props, name);
+		} catch (Exception e) {
+			System.err.println("Error during connection.");
+		}
+
+		ClassConfigurator.add(Common.TYPE_HEADER_MAGIC_ID, TypeOfMessageHeader.class);
+
+		// Configure destination address of our messages
+		if (view != null && view.getMembers().size() > 1) {
+			if (addr != null){
+				for (int i = 0; i < view.getMembers().size() && destAddress == null; i++){
+					if (view.getMembers().get(i).toString().equals(addr))
+						destAddress = view.getMembers().get(i);
+				}
+			} else {
+				destAddress = view.getMembers().get(0);
+			}
+		} else {
+			System.err.println("There aren't nodes in the DHT cluster.");
+		}
 	}
 	
-	public static DHT getInstance(){
+	public static DHT getInstance(String addr){
 		if (instance == null)
-			instance = new DHT();
+			instance = new DHT(addr);
 		
 		return instance;
 	}
 	
 	public  void getData() {
-		System.out.println("Id of the data you want to get: ");
+		System.out.printf("Id of the data you want to get: ");
 		String id = scanner.nextLine();
 		int key = Math.abs(id.hashCode() % Common.MAX_ADDR);
-		System.out.println(key);
-		sendGetDataMessage (channel.getAddress(), view.getMembers().get(0), key);
-	}
-	
-	public  void putData(){
-		
+		sendGetDataMessage (channel.getAddress(), destAddress, key);
 	}
 	
 	private void sendGetDataMessage (Address srcAddress, Address destAddress, int key){
@@ -117,6 +136,29 @@ public class DHT extends ReceiverAdapter {
 	
 	private Data dataReceived (DataMessage dataMsg){
 		return dataMsg.getData();
+	}
+	
+	public  void putData(){
+		System.out.printf("Id of the data you want to put: ");
+		String id = scanner.nextLine();
+		System.out.printf("Content of the data you want to put: ");
+		String content = scanner.nextLine();
+		
+		Data data = new Data (id, content);
+		
+		sendPutDataMessage (destAddress, data);
+		
+		System.out.println("\n" + data);
+	}
+	
+	private void sendPutDataMessage (Address address, Data data){
+		Message msg=new Message(address, new PutDataMessage(data));
+		msg.putHeader(Common.TYPE_HEADER_MAGIC_ID, new TypeOfMessageHeader(Common.PUT_DATA));
+		try{
+			channel.send(msg);
+		} catch (Exception e) {
+			System.err.println("Error sending PutDataMessage.");
+		}
 	}
 	
 	public void viewAccepted (View new_view) {
@@ -136,14 +178,14 @@ public class DHT extends ReceiverAdapter {
 	public void receive(Message msg) {
 
 		int type = ((TypeOfMessageHeader) msg.getHeader(Common.TYPE_HEADER_MAGIC_ID)).getType();
-		DHT dht = DHT.getInstance();
+		DHT dht = DHT.getInstance(null);
 
 		synchronized (dht){
 			switch (type){
 			case Common.DATA:
 				Data dataReceived = dataReceived((DataMessage) msg.getObject());
 				if (dataReceived != null)
-					System.out.println(dataReceived);
+					System.out.println("\n" + dataReceived);
 				else
 					System.out.println("The requested data doesn't exist in the DHT cluster.");
 				dht.notify();
